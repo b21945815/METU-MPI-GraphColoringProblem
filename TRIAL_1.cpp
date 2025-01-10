c8799201dd1193aba6a552e3bbb17a52a16fe66a
fatih656795 <f.ay@tryoto.com>
2025-01-10 15:48:18 +0300

    update

--- TRIAL_1.cpp
+++ TRIAL_1.cpp
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     if (rank == 0) {
-        //runSequentialAlgorithm();
+        runSequentialAlgorithm();
     }
     MPI_Barrier(MPI_COMM_WORLD);
     Algorithm1Basic::runAlgorithm1Basic();
